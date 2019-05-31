56c1d032d7c422f99e8d8ba0c69351ccfb01687b
tnantoka <tnantoka@bornneet.com>
2019-05-31 22:44:31 +0900

    publish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math.dart' hide Colors;
 
 void main() => runApp(MyApp());
 
@@ -17,20 +19,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Offset _startPosition = const Offset(0, 0);
+  double _currentrRotation = 0;
+  double _lastRotation = 0;
+
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final Offset center = Offset(size.width * 0.5, size.height * 0.5);
     return Scaffold(
-      body: Center(
-        child: CustomPaint(painter: _MyPainter()),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        child: Center(
+          child: CustomPaint(
+            painter: _MyPainter(
+              rotation: _lastRotation + _currentrRotation,
+            ),
+          ),
+        ),
+        onPanStart: (DragStartDetails details) {
+          _startPosition = details.globalPosition;
+        },
+        onPanUpdate: (DragUpdateDetails details) {
+          final Offset updatedPosition = details.globalPosition;
+
+          final Vector2 a = Vector2(
+            _startPosition.dx - center.dx,
+            _startPosition.dy - center.dy,
+          );
+          final Vector2 b = Vector2(
+            updatedPosition.dx - center.dx,
+            updatedPosition.dy - center.dy,
+          );
+
+          final double dot = dot2(a, b);
+          final double cross = cross2(a, b);
+
+          final double radian = atan2(cross, dot);
+          // double degree = degrees(radian);
+
+          setState(() {
+            _currentrRotation = radian;
+          });
+        },
+        onPanEnd: (DragEndDetails details) {
+          setState(() {
+            _lastRotation += _currentrRotation;
+            _currentrRotation = 0;
+          });
+        },
       ),
     );
   }
 }
 
 class _MyPainter extends CustomPainter {
+  _MyPainter({this.rotation});
+  double rotation;
+
   @override
   bool shouldRepaint(_MyPainter oldDelegate) {
-    return false;
+    return rotation != oldDelegate.rotation;
   }
 
   @override
@@ -40,6 +89,9 @@ class _MyPainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 3;
     final Rect rect = Rect.fromCircle(center: const Offset(0, 0), radius: 100);
+    canvas.save();
+    canvas.rotate(rotation);
     canvas.drawRect(rect, stroke);
+    canvas.restore();
   }
 }
